fac4d2e89ade293ad907cfa2cac895bba896476f
pedrin <pedrinho.top07@gmail.com>
2026-02-19 09:36:47 -0300

    inital commit

---
+++ src/main/java/com/fiap/hackNotification/HackNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.hackNotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/hackNotification/HackNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.hackNotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
